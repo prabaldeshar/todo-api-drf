a47d298514a7488b0f17f26c62c38577d9331b8b
Prabal Deshar <prabaldeshar@gmail.com>
2021-09-22 22:51:29 +0545

    Added the frontend app for the UI and added functions to fetch data from api

--- api/views.py
+++ api/views.py
@@ -21,7 +21,7 @@ def apiOverView(request):
 
 @api_view(['GET'])
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-id')
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
 
@@ -49,7 +49,7 @@ def taskUpdate(request, pk):
         serializer.save()
     
     return Response(serializer.data)
-
+    path('task-update/<str:pk>/', views.taskUpdate, name="task-update"),
 @api_view(['DELETE'])
 def taskDelete(request, pk):
     task = Task.objects.get(id=pk)
